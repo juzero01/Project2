1f05b786cfaa75afd3d6f8b2d8668b123d6e14d3
hjkwon <revo3325@gmail.com>
2025-05-09 09:53:08 -0700

    filetime1, filetime2 함수 구현

---
+++ main.c
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+
+struct stat stat1, stat2;
+struct tm *time1, *time2;
+
+void filestat1();
+void filestat2();
+void filetime1();
+void filetime2();
+void sizecmp();
+void blockcmp();
+void datecmp();
+void timecmp();
+
+int main(){
+    filestat1();
+    filestat2();
+    filetime1();
+    filetime2();
+    sizecmp();
+    blockcmp();
+    datecmp();
+    timecmp();
+}
+
+//파일 1의 정보를 가져오는 함수 작성
+void filestat1(){
+    
+}
+
+//파일 2의 정보를 가져오는 함수 작성
+void filestat2(){
+    
+}
+
+//파일 1의 시간 정보를 가져오는 함수 작성
+void filetime1(){
+    time1=localtime(&stat1.st_mtime); // 마지막 수정 시간 → 로컬 시간(time1) 저장
+    if (time1 == NULL) {
+        perror("localtime() 변환 실패");
+        return;
+    }
+}
+
+//파일 2의 시간 정보를 가져오는 함수 작성
+void filetime2(){
+    time2=localtime(&stat2.st_mtime); // 마지막 수정 시간 → 로컬 시간(time2) 저장
+    if (time2 == NULL) {
+        perror("localtime() 변환 실패");
+        return;
+    }
+}
+
+//두 개의 파일 크기를 비교하는 함수 작성
+void sizecmp(){
+    
+}
+
+//두 개의 파일 블락 수를 비교하는 함수 작성
+void blockcmp(){
+    
+}
+
+//두 개의 파일 수정 날짜를 비교하는 함수 작성
+void datecmp(){
+    
+}
+
+//두 개의 파일 수정 시간을 비교하는 함수 작성
+void timecmp(){
+    
+}
